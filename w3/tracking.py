a03d128337cee637ae532d67706845bd2d2cccbd
cesc47 <francescnet@gmail.com>
2022-03-28 21:43:52 +0200

    haria todas las funciones del tracking en tracking.py

--- w3/tracking.py
+++ w3/tracking.py
@@ -1,5 +1,19 @@
+import uuid
+
 def tracking_overlap():
+    # todo: cambiar update diccionario labels y añadir identifiers
+    # todo: algoritmo que mire las bboxes y seleccione mayor iou
+    # todo: mejorarlo con ideas
     print('todo')
+
 
 def tracking_kalman():
     print('todo')
+
+
+def create_id():
+    """
+    creates unique identifier
+    :return: identifier
+    """
+    return uuid.uuid4()
