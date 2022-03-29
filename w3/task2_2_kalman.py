c868b7b9abdfbd7b5c70370ff60cabe18776723a
David Serrano <99d.serrano@gmail.com>
2022-03-29 20:23:22 +0200

    kalman done

--- w3/task2_2_kalman.py
+++ w3/task2_2_kalman.py
@@ -1,7 +1,9 @@
 from sort.sort import Sort
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+from PIL import Image
 
 from dataset_gestions import load_labels, get_frames_paths
 
@@ -24,9 +26,10 @@ if __name__ == "__main__":
 
     frame_id = int(list(detections.keys())[0])
     ending_frame = int(list(detections.keys())[-1]) - 1
-    print(f'Starting in the frame {frame_id} until {ending_frame}')
+    print(f'Starting in the frame {frame_id} until {ending_frame}\n')
 
-    for idx in range(frame_id, ending_frame):
+    output_frames = []
+    for idx in tqdm(range(frame_id, ending_frame), desc='Computing tracking'):
         id_seen = []
 
         # Obtain the gt and detected bboxes of the current frame
@@ -39,9 +42,17 @@ if __name__ == "__main__":
         # Read the current frame
         frame = cv2.imread(frames[idx])
 
+        os.makedirs('kalman_frames', exist_ok=True)
+
         # Draw the detected bboxes and track ids
         for t in trackers:
             cv2.rectangle(frame, (int(t[0]), int(t[1])), (int(t[2]), int(t[3])), (0, 0, 255), 2)
             cv2.putText(frame, str(int(t[4])), (int(t[0]), int(t[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.imshow('frames', frame)
-        cv2.waitKey(1)
+
+        cv2.imwrite(f'kalman_frames/{idx:04}.jpg', frame)
+        if idx-frame_id > 500:
+            output_frames.append(Image.fromarray(frame[:,:,::-1]))
+
+    print('generating GIF...')
+    frame_one = output_frames[0]
+    frame_one.save(fp='kalman.gif', format="GIF", append_images=output_frames, save_all=True, duration=50, loop=0)
